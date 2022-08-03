6b403b96355486dac9205e8f55aa65331919f68e
euclides981 <euclides981@gmail.com>
2022-08-03 16:13:32 -0300

    not

--- app/public/js/teste.js
+++
@@ -1,9 +0,0 @@
-const carros = {
-    marca: 'GM',
-    cor: 'Prata',
-    ano: '1997',
-}
-
-const createCarros = (veiculos) => {
-    localStorage.setItem('db_carros', JSON.stringify(veiculos))
-}
